904b472eed7326074bb5cb777eb66ce3b2b10a7f
pauletteandaverde <paulette.andaverde01@utrgv.edu>
2025-02-01 19:26:16 +0000

    First commit

--- src/MixMatch.java
+++ src/MixMatch.java
@@ -1,36 +1,36 @@
-public class MixMatch {
-    // Main method here
+public class MixMatch{
     public static void main(String[] args){
-        // String
+        // String variable
         String varString = "text";
         System.out.println(varString + " <- String, stores a sequence of characters");
 
-        // char
+        // char variable
         char varChar = '#';
         System.out.println(varChar + " <- Char, stores a single character");
 
-        // short 
+        // short variable
         short varShort = 100;
         System.out.println(varShort + " <- Short, stores values from -32,768 to 32,767");
 
-        // int 
+        // int variable
         int varInt = -40000;
-        System.out.println(varInt + " <- Integer, stores values from -2,147,483,648 to 2,147,483,647");
+        System.out.println(varInt + " <- Integer, stores values from -(2^32) to 2^32-1");
 
-        // long 
-        long varLong = 1777;
+        // long variable
+        long varLong = 1777777;
         System.out.println(varLong + " <- Long, stores values from -(2^64) to 2^64-1");
 
-        // float 
+        // float variable
         float varFloat = 12.56f;
         System.out.println(varFloat + " <- Float, occupies 32 bits (4 bytes) of memory");
 
-        // double 
+        // double variable
         double varDouble = 16.8998;
         System.out.println(varDouble + " <- Double, occupies 64 bits (8 bytes) of memory");
 
-        // boolean
+        // boolean variable
         boolean varBoolean = false;
         System.out.println(varBoolean + " <- Boolean, stores only two values: true or false");
+
     }
 }
